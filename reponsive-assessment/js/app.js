d72fe1867e80da619929e82affb92e0554969b14
Marycynthia23 <69634281+Marycynthia23@users.noreply.github.com>
2021-04-11 17:23:06 +0100

    Add files via upload

---
+++ reponsive-assessment/js/app.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelector('navLinks');
+const closeBtn = document.querySelector('closeBtn');
+const navMenu = document.querySelector('navMenu');
+
+
+
+navMenu.addEventListener('click' show);
+closeBtn.addEventListener('click' close);
